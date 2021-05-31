fc4f10ec23965d76b79745474b7a68e3763e2761
alexey.firsov <tehpod.prg9@szemi.ru>
2021-05-31 13:28:47 +0400

    Домашнее задание к занятию № 2 небольшие правки, добавлены описания

--- Assets/Scripts/Transports/Accessories/Engine.cs
+++ Assets/Scripts/Transports/Accessories/Engine.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class Engine : MonoBehaviour
 {
-    [Range(0.0f, 100.0f)]
-    [SerializeField] private float mass = 15.0f;
-
-    public float Mass { get => mass; set => mass = value; }
+   
 }
--- Assets/Scripts/Transports/Accessories/Tower.cs
+++ Assets/Scripts/Transports/Accessories/Tower.cs
@@ -10,12 +10,8 @@ namespace Race
         [SerializeField] private Transform m_MainWeaponPointOfShot;
         [SerializeField] private Transform m_SecondaryWeaponPointOfShot;
 
-        [Range(0.0f, 100.0f)]
-        [SerializeField] private float mass = 15.0f;
-
         public Transform MainWeaponPointOfShot { get => m_MainWeaponPointOfShot; private set => m_MainWeaponPointOfShot = value; }
         public Transform SecondaryWeaponPointOfShot { get => m_SecondaryWeaponPointOfShot; private set => m_SecondaryWeaponPointOfShot = value; }
         public List<Transform> SpawnDynamicArmor { get => m_SpawnDynamicArmor; set => m_SpawnDynamicArmor = value; }
-        public float Mass { get => mass; set => mass = value; }
     }
 }
