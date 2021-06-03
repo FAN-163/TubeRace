d104c7ef7d647ea85f1016d3e7ad82a9dd055337
alexey.firsov <tehpod.prg9@szemi.ru>
2021-06-03 19:53:28 +0400

    Домашнее задание к занятию №4

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -10,23 +10,57 @@ namespace Race
         [SerializeField] private float m_RollAngle;
         [SerializeField] private float m_Distance;
 
+        [Range(-100.0f, 100.0f)]
+        [SerializeField] private float m_RollSpeed;
+
         [Range(0.0f, 20.0f)]
         [SerializeField] private float m_RadiusModifier;
+
+        [SerializeField] private bool OnItsAxis;
+
+        private float m_MaxRollAngle = 360f;
+
         private void OnValidate()
+        {
+            SetObstaclePosition();
+        }
+        private void Update()
         {
             SetObstaclePosition();
         }
 
         private void SetObstaclePosition()
         {
+            float dt = Time.deltaTime;
+
             Vector3 obstaclePos = m_Track.GetPosition(m_Distance);
             Vector3 obstacleDir = m_Track.GetDirection(m_Distance);
 
+            m_RollAngle += m_RollSpeed * dt;
+
+            //обнуляет значение когда оно больше 360 или меньше -360
+            //что бы не росло/уменьшалось до максимальных/минимальных значений float
+            if(m_RollAngle >= m_MaxRollAngle || m_RollAngle <= -m_MaxRollAngle)
+            {
+                m_RollAngle = 0.0f;
+            }
+            
             Quaternion q = Quaternion.AngleAxis(m_RollAngle, Vector3.forward);
             Vector3 trackOffset = q * (Vector3.up * (m_RadiusModifier * m_Track.Radius));
 
-            transform.position = obstaclePos - trackOffset;
-            transform.rotation = Quaternion.LookRotation(obstacleDir, trackOffset);
+            
+           
+            //Вращение вокруг трека или вокруг собственной оси
+            if (!OnItsAxis)
+            {
+                transform.position = obstaclePos - trackOffset;
+                transform.rotation = Quaternion.LookRotation(obstacleDir, trackOffset);
+            }
+            else
+            {
+                transform.Rotate(transform.forward * m_RollSpeed * dt);
+            }
+
         }
 
         private void OnDrawGizmos()
