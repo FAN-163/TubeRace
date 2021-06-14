64080dd3dd0183c7ed5840729964e4342e56cac0
FAN-404 <f.a.n.163@mail.ru>
2021-06-14 09:50:53 +0400

    Revert "Домашнее задание к занятию № 6 (генерирование объекта с определенным шансом)"
    This reverts commit 31d480427504f7973934ab3be7be2b39e81b212a.

--- Assets/Scripts/ObjectPlacer.cs
+++ Assets/Scripts/ObjectPlacer.cs
@@ -11,15 +11,7 @@ namespace Race
         [SerializeField] private int m_NumObjects;
         [SerializeField] private RaceTrack m_Tarck;
 
-
-        [Header("1 - 100%, 2 - 50%, 3 - 30% ")]
-        [Range(1, 3)]
-        [SerializeField] private int m_ChanceGenerateObject = 1;
-
         [SerializeField] private bool m_RandomizeRotation;
-        [SerializeField] private bool m_RandomizePosition;
-
-        private GameObject prefab;
 
         private void Start()
         {
@@ -27,32 +19,16 @@ namespace Race
 
             for (int i = 0; i < m_NumObjects; i++)
             {
+                var e = Instantiate(m_Prefab);
 
-                float random = UnityEngine.Random.Range(0, 100);
+                e.transform.position = m_Tarck.GetPosition(distance);
+                e.transform.rotation = m_Tarck.GetRotation(distance);
 
-                if (m_RandomizePosition)
+                if(m_RandomizeRotation)
                 {
-                    if (random % m_ChanceGenerateObject == 0)
-                    {
-                        prefab = Instantiate(m_Prefab);
-                    }
+                    e.transform.Rotate(Vector3.forward, UnityEngine.Random.Range(0, 360), Space.Self);
                 }
-                else
-                {
-                    prefab = Instantiate(m_Prefab);
-                }
-
-                if (prefab)
-                {
-                    prefab.transform.position = m_Tarck.GetPosition(distance);
-                    prefab.transform.rotation = m_Tarck.GetRotation(distance);
-                    
-                    if (m_RandomizeRotation)
-                    {
-                        prefab.transform.Rotate(Vector3.forward, UnityEngine.Random.Range(0, 360), Space.Self);
-                    }
-                }
-
+                
                 distance += m_Tarck.GetTrackLength() / m_NumObjects;
             }
         }
