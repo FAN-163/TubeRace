5a0d6d02ccec80ff2beb4d760b37b52389b10996
FAN-404 <f.a.n.163@mail.ru>
2021-06-25 14:08:09 +0400

    домашнее задание к занятию № 9 (изменение разрешения экрана), добавлены кнопки возврата в главное меню

--- Assets/Scripts/OptionsViewController.cs
+++ Assets/Scripts/OptionsViewController.cs
@@ -7,21 +7,12 @@ namespace Race
 {
     public class OptionsViewController : MonoBehaviour
     {
+        [SerializeField] private MainMenuViewController m_MainMenuViewController;
         [SerializeField] private Dropdown m_Dropdown;
         [SerializeField] private bool m_IsFullScreen = true;
 
         private readonly int[] m_Height = new int[] {576, 720, 1080};
         private readonly int[] m_Wigth = new int[] {720, 1280, 1920};
-
-        //private void Update()
-        //{
-        //    Dropdown.OptionData optionData = new Dropdown.OptionData();
-        //    optionData.text = "";
-        //    m_Dropdown.options.Add(optionData);
-        //    Debug.Log(m_Dropdown.value);
-        //    Debug.Log(m_Dropdown.captionText.text);
-
-        //}
 
         private void Awake()
         {
@@ -30,6 +21,7 @@ namespace Race
             Screen.SetResolution(m_Wigth[0], m_Height[0], m_IsFullScreen);
         }
 
+        //при изменении значения дропдаун, меняет разрешение
         public void SetScreenResolution()
         {
             int optionValue = m_Dropdown.value;
@@ -37,6 +29,7 @@ namespace Race
             Screen.SetResolution(m_Wigth[optionValue], m_Height[optionValue], m_IsFullScreen);
         }
 
+        //создает в дропдаут список имеющихся разрешений
         private void SetValueDropdown()
         {
             for (int i = 0; i < m_Height.Length; i++)
@@ -45,6 +38,13 @@ namespace Race
                 optionData.text = $"{m_Wigth[i]} x {m_Height[i]}";
                 m_Dropdown.options.Add(optionData);
             }
+        }
+
+        public void OnButtonExit()
+        {
+            gameObject.SetActive(false);
+
+            m_MainMenuViewController.gameObject.SetActive(true);
         }
     }
 }
--- Assets/Scripts/TrackSelectionViewController.cs
+++ Assets/Scripts/TrackSelectionViewController.cs
@@ -8,9 +8,18 @@ namespace Race
 
     public class TrackSelectionViewController : MonoBehaviour
     {
+        [SerializeField] private MainMenuViewController m_MainMenuViewController;
+
         private void Awake()
         {
             gameObject.SetActive(false);
+        }
+
+        public void OnButtonExit()
+        {
+            gameObject.SetActive(false);
+
+            m_MainMenuViewController.gameObject.SetActive(true);
         }
     }
 }
